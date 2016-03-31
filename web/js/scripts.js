17dfdae444a638c4cd82beed83fbf0c0b769675f
super1002 <flexiway.studio@gmail.com>
2016-03-31 19:17:35 +0200

    added css, js, imgs \& fonts!

---
+++ web/js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('select').material_select();
+});
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+});
+
+
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
