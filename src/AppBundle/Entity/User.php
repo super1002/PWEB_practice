eafd2abf1b7998bbef27ae3efd2d25a8bbc7c812
super1002 <flexiway.studio@gmail.com>
2016-04-05 18:53:38 +0200

    Server side assertion for user class done

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,16 +2,29 @@
 
 namespace AppBundle\Entity;
 
+use AppBundle\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
-use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Validator;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * User
  *
  * @ORM\Table(name="users")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ * @UniqueEntity(
+ *     fields={"email"},
+ *     message="This email is already registered")
+ * @UniqueEntity(
+ *     fields={"username"},
+ *     message="This username is not available")
+ *
  */
-class User extends BaseUser
+class User implements UserInterface, AdvancedUserInterface
 {
     /**
      * @var int
@@ -32,6 +45,70 @@ class User extends BaseUser
      * @ORM\Column(name="twitter", type="string", length=255)
      */
     protected $twitter_user;
+
+    /**
+     * @ORM\Column(name="username", type="string", length=25, unique=true)
+     *
+     * @Validator\Length(
+     *      min = 6,
+     *      max = 25,
+     *      minMessage = "Your username must be at least {{ limit }} characters long",
+     *      maxMessage = "Your username cannot be longer than {{ limit }} characters"
+     * )
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(name="salt", type="string", length=80)
+     */
+    private $salt;
+
+    /**
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var String
+     * @Validator\Length(
+     *      min = 6,
+     *      max = 10,
+     *      minMessage = "Your password must be at least {{ limit }} characters long",
+     *      maxMessage = "Your password cannot be longer than {{ limit }} characters"
+     * )
+     */
+    private $plain_password;
+
+    /**
+     * @ORM\Column(name="email", type="string", length=60, unique=true)
+     * @Validator\Email()
+     */
+    private $email;
+
+    /**
+     * @ \ArrayCollection
+     * @ORM\Column(name="roles", type="array")
+     */
+    private $roles;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    public function __construct()
+    {
+        $this->isActive = false;
+        $this->salt = base_convert(sha1(uniqid(mt_rand(), true)), 16, 36);
+        $this->roles = new ArrayCollection();
+        $this->roles->add("ROLE_USER");
+
+        $this->enabled = false;
+        $this->locked = false;
+        $this->expired = false;
+        $this->credentialsExpired = false;
+    }
+
 
     /**
      * @return String
@@ -59,6 +136,97 @@ class User extends BaseUser
         return $this->id;
     }
 
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    public function isEnabled()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPlainPassword()
+    {
+        return $this->plain_password;
+    }
+
+    /**
+     * @param mixed $plain_password
+     */
+    public function setPlainPassword($plain_password)
+    {
+        $this->plain_password = $plain_password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @param mixed $isActive
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+    }
 
 
 }
