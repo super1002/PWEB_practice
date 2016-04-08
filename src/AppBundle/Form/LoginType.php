4da02ef2150f2e84839f79f6837a9a2c90a25986
super1002 <flexiway.studio@gmail.com>
2016-04-08 19:50:32 +0200

    Login form fixed

--- src/AppBundle/Form/LoginType.php
+++ src/AppBundle/Form/LoginType.php
@@ -4,6 +4,7 @@
 namespace AppBundle\Form;
 
 
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -11,19 +12,36 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+
 
 class LoginType extends AbstractType{
+
+
+    protected $router;
+
+    public function __construct(UrlGeneratorInterface  $router)
+    {
+        $this->router = $router;
+    }
+
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            //->setAction(generateUrl('login_check')) //must inject router service for this to work
+            ->setAction($this->router->generate('login_check')) //must inject router service for this to work
             ->add('username', TextType::class)
             ->add('password', PasswordType::class)
             ->add('submit', SubmitType::class)
+            ->add('_target_path', HiddenType::class, array(
+                'data' => '/'
+            ))
+
         ;
 
     }
+
 
     public function getBlockPrefix()
     {
