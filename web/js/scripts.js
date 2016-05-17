0a98b2747f1741f1619204d1d67a393f06cc7b08
super1002 <flexiway.studio@gmail.com>
2016-05-17 19:55:51 +0200

    Now resized pictures are selected. Before deleting a product in "My Products" page, a confirmation modal appears

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -46,3 +46,8 @@ function submitSearch(){
     $('button.hidden').click();
 
 }
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+});
